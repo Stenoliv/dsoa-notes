fb43ba48f0668981e8aab970e1a8b4130c37b9c4
stenbaco <stenbaco@arcada.fi>
2024-01-08 11:37:27 +0200

    Notes 8.1.24 11:37

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Helljo ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+       Car car = new Car("Aston-Martin");
+       Car car2 = new Car("Alfa-Romeo");
+
+       System.out.println(car.getName());
+       System.out.println(car2.getName());
     }
 }
