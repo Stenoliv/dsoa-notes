d5d07966a98b44499ebaf3f0d397e82b470a8c54
Oliver Stenbacka <stenbaco@arcada.fi>
2024-01-18 11:06:39 +0200

    CC3?

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +15,22 @@ public class Main {
        System.out.println(car2.getName());
 
        System.out.println( Calculator.multiplyMethod(2,3));
+
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Your random number is: " + random.ints(1 , 100).findFirst().getAsInt());
+
+            System.out.println("Press Enter for a new number or Write q to quit");
+            if(sc.nextLine().equals("q")){
+                System.out.println("Good bye!");
+                System.exit(0);
+
+            }
+        }
+
+
+
     }
 }
